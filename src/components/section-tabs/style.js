6be614b6b3a43eea111ee5b8b5f81c5b897588a2
wangjian <wangjian_1998@outlook.com>
2022-11-15 18:16:47 +0800

    文本超出不换行 white-space: nowrap

--- src/components/section-tabs/style.js
+++ src/components/section-tabs/style.js
@@ -7,12 +7,14 @@ export const TabsWrapper = styled.div`
 
     .tablist {
       flex-basis: 120px;
+      flex-shrink: 0;
       padding: 14px 16px;
       margin-right: 10px;
       border: solid 0.5px #D8D8D8;
       border-radius: 3px;
       font-size: 17px;
       font-weight: bold;
+      white-space: nowrap;
       cursor: pointer;
       text-align: center;
       ${props=>props.theme.minix.boxShadow}
