1020b9dab0b98859a2953cc2c2a478097b954ae1
wangjian <wangjian_1998@outlook.com>
2022-11-24 12:03:38 +0800

    页面切换时页面回到顶部

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { memo } from 'react'
-import {useRoutes} from 'react-router-dom'
+import React, { memo, useEffect } from 'react'
+import {useLocation, useRoutes} from 'react-router-dom'
 import routes from '@/router'
 import AppHeader from './components/app-header'
 import AppFooter from './components/app-footer'
@@ -11,6 +11,11 @@ const App = memo(() => {
   const {headerConfig} = useSelector(state => ({
     headerConfig: state.main.headerConfig
   }), shallowEqual)
+
+  const location = useLocation()
+  useEffect(() => {
+    window.scrollTo(0,0)
+  }, [location.pathname])
   return (
     <div>
       <div className='header'>
