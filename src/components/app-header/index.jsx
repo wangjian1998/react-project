92c5f61238f6259355388b4a7691a0081606beaa
wangjian <wangjian_1998@outlook.com>
2022-11-24 15:34:10 +0800

    头部监听滚动效果的消失

--- src/components/app-header/index.jsx
+++ src/components/app-header/index.jsx
@@ -1,10 +1,11 @@
-import React, { memo, useState } from 'react'
+import React, { memo, useRef, useState } from 'react'
 import {HeaderWrapper, SearchAreaWrapper} from './style'
 import HeaderLeft from './c-cpns/header-left'
 import HeaderCenter from './c-cpns/header-center'
 import HeaderRight from './c-cpns/header-right'
 import { useSelector, shallowEqual } from 'react-redux'
 import ClassNames from 'classnames'
+import useScrollPosition from '@/hooks/useScrollPosition'
 
 const AppHeader = memo(() => {
 
@@ -13,8 +14,15 @@ const AppHeader = memo(() => {
   const {headerConfig} = useSelector(state => ({
     headerConfig: state.main.headerConfig
   }), shallowEqual)
-
   const {isFixed} = headerConfig
+
+  // 获取屏幕滚动值
+  const {scrollY} = useScrollPosition()
+  const prevY = useRef()
+  // 在正常情况下（搜索框没有弹出），不断记录值
+  if(!isSearch) prevY.current = scrollY
+  // 在弹出搜索功能情况下，滚动的距离大于之前记录距离的30
+  if(isSearch && Math.abs(scrollY - prevY.current) > 30) setIsSearch(false)
   return (
     <HeaderWrapper className={ClassNames({fixed: isFixed})}>
       <div className="content">
